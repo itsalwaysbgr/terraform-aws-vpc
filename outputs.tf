40f99152b117feb4fc7cd15ba6d4afbca3469e37
nane <gnane5940@gmail.com>
2025-07-03 17:39:09 +0530

    2025-jul-03

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,11 @@ output "vpc_id" {
 output "public_subnet_ids" {
     value = aws_subnet.public[*].id
 }
+
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
