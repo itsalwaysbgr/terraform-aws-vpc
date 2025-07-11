8c81803f07a778fa592197bd1f17607fe6ae0eef
nane <gnane5940@gmail.com>
2025-07-11 23:56:25 +0530

    2025-jul-11

--- vpc.tf
+++ vpc.tf
@@ -166,7 +166,7 @@ resource "aws_route" "public" {
 resource "aws_route" "private" {
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.main.id
+  gateway_id             = aws_nat_gateway.main.id
 }
 
 
